851795191db40bfabbe9c75beda6ba5cd447f873
Roberts Ivanovs <roberts.ivanovs1999@gmail.com>
2020-09-19 22:49:06 +0300

    fixed some code

--- backend/copernicus/utils.py
+++ backend/copernicus/utils.py
@@ -6,31 +6,56 @@ from PIL import Image
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 
-
-def __myHistogram(image):
-    histogram = np.zeros(256)
-    for i in range(0, 256):
-        maskPixels_i = image == i
-        histogram[i] = np.sum(maskPixels_i)
-
-    x = np.arange(256)
-    return histogram
+from zipfile import ZipFile
+import io
+import zipfile
 
 
-def __set_hashmap(list_of_values):
-    hashmap_result = {}
-    for i in range(len(list_of_values)):
-        hashmap_result[str(i)] = list_of_values[i]
+class InMemoryZip:
+    """Inspiration from: https://stackoverflow.com/a/2463818/11000105"""
 
-    return hashmap_result
+    def __init__(self):
+        self.in_memory_zip = io.BytesIO()
+
+    def append(self, filename_in_zip, file_contents=""):
+        """Appends a file with name filename_in_zip and contents of
+        file_contents to the in-memory zip."""
+        # Get a handle to the in-memory zip in append mode
+        with zipfile.ZipFile(
+            self.in_memory_zip, "a", zipfile.ZIP_DEFLATED, False
+        ) as zip_file:
+            zip_file.writestr(filename_in_zip, file_contents)
+
+        return self
+
+    def read(self):
+        """Returns a string with the contents of the in-memory zip."""
+        self.in_memory_zip.seek(0)
+        return self.in_memory_zip
 
 
-def return_map_from_img(image):
-    f2Int = 255 - np.round(image * 255)
-    f2Int = f2Int.astype(np.uint8)
-    histogram = __myHistogram(f2Int)
-    hashmap = __set_hashmap(histogram)
-    return hashmap
+def dry_percentage_hashmap(image):
+    T = np.arange(-0.8, 0.8)
+    T = np.asarray(T)
+    border_dry = -0.2
+    border_semi_dry = 0.2
+    result_list = [0, 0, 0]
+    for i in range(len(image)):
+        for j in range(len(image[0])):
+            if image[i][j] <= border_dry:
+                result_list[0] += 1
+            elif image[i][j] > border_dry and image[i][j] < border_semi_dry:
+                result_list[1] += 1
+            elif image[i][j] >= border_semi_dry:
+                result_list[2] += 1
+
+    result_hashmap = {
+        "dry": int(result_list[0] / image.size * 100),
+        "semidry": int(result_list[1] / image.size * 100),
+        "moist": int(result_list[2] / image.size * 100),
+    }
+
+    return result_hashmap
 
 
 client_id = "97d66040-c9d0-4d7b-903e-aac596adde74"
@@ -48,10 +73,26 @@ token = oauth.fetch_token(
 resp = oauth.get("https://services.sentinel-hub.com/oauth/tokeninfo")
 
 
-def get_image(payload):
-    response = oauth.post(
-        "https://services.sentinel-hub.com/api/v1/process",
-        json=payload,
+def get_image(payload, filename):
+    oauth.fetch_token(
+        token_url="https://services.sentinel-hub.com/oauth/token",
+        client_id=client_id,
+        client_secret=client_secret,
     )
-    array = np.frombuffer(response.content, dtype=np.uint8)
-    return array
+    # All requests using this session will have an access token automatically added
+    resp = oauth.get("https://services.sentinel-hub.com/oauth/tokeninfo")
+
+    response = oauth.post(
+        "https://services.sentinel-hub.com/api/v1/process", json=payload,
+    )
+    with open(filename, "wb") as target:
+        target.write(response.content)
+
+    pic = imread(filename)
+    return pic
+
+
+def extract_data(imageB8A, imageB11):
+    result = (imageB8A - imageB11) / (imageB8A + imageB11)
+    hashmap = dry_percentage_hashmap(result)
+    return hashmap
--- backend/copernicus/views.py
+++ backend/copernicus/views.py
@@ -1,21 +1,21 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
-from copernicus.utils import get_image, return_map_from_img
+from copernicus.utils import get_image, extract_data
 
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 @csrf_exempt
 def get_picture_data(request):
-    payload = {
+    payloadB8A = {
         "input": {
             "bounds": {
                 "bbox": [
-                    2877643.9131091023,
-                    7666310.225629501,
-                    2887771.8193568876,
-                    7679189.8648955505,
+                    1244700.56859581,
+                    5100795.771526381,
+                    1538218.757210887,
+                    5191450.0870726025,
                 ],
                 "properties": {"crs": "http://www.opengis.net/def/crs/EPSG/0/3857"},
                 "geometry": None,
@@ -24,8 +24,8 @@ def get_picture_data(request):
                 {
                     "dataFilter": {
                         "timeRange": {
-                            "from": "2018-06-09T00:00:00.000Z",
-                            "to": "2018-06-09T23:59:59.999Z",
+                            "from": "2020-09-17T00:00:00.000Z",
+                            "to": "2020-09-17T23:59:59.999Z",
                         },
                         "mosaickingOrder": "mostRecent",
                         "previewMode": "EXTENDED_PREVIEW",
@@ -37,15 +37,51 @@ def get_picture_data(request):
             ],
         },
         "output": {
-            "width": 1012,
-            "height": 1287,
+            "width": 625,
+            "height": 193,
             "responses": [{"identifier": "default", "format": {"type": "image/tiff"}}],
         },
         "evalscript": '//VERSION=3\nfunction setup() {\n  return {\n    input: ["B8A"],\n    output: { bands: 1, sampleType: "UINT8" }\n  };\n}\n\nfunction evaluatePixel(sample) {\n  return [255 * sample.B8A ];}',
     }
 
-    # image = get_image(payload)
-    # hashmap = return_map_from_img(image)
-    # print(image)
+    payloadB11 = {
+        "input": {
+            "bounds": {
+                "bbox": [
+                    1244700.56859581,
+                    5100795.771526381,
+                    1538218.757210887,
+                    5191450.0870726025,
+                ],
+                "properties": {"crs": "http://www.opengis.net/def/crs/EPSG/0/3857"},
+                "geometry": None,
+            },
+            "data": [
+                {
+                    "dataFilter": {
+                        "timeRange": {
+                            "from": "2020-09-17T00:00:00.000Z",
+                            "to": "2020-09-17T23:59:59.999Z",
+                        },
+                        "mosaickingOrder": "mostRecent",
+                        "previewMode": "EXTENDED_PREVIEW",
+                        "maxCloudCoverage": 100,
+                    },
+                    "processing": {"upsampling": "BICUBIC"},
+                    "type": "S2L2A",
+                }
+            ],
+        },
+        "output": {
+            "width": 625,
+            "height": 193,
+            "responses": [{"identifier": "default", "format": {"type": "image/tiff"}}],
+        },
+        "evalscript": '//VERSION=3\nfunction setup() {\n  return {\n    input: ["B11"],\n    output: { bands: 1, sampleType: "UINT8" }\n  };\n}\n\nfunction evaluatePixel(sample) {\n  return [255 * sample.B11 ];}',
+    }
 
-    return JsonResponse({"dry": 50., "semidry": 45., "moist": 5.})
+    parsed_image_B8A = get_image(payloadB8A, "payloadB8A.tiff")
+    parsed_image_B11 = get_image(payloadB11, "payloadB11.tiff")
+
+    map_data = extract_data(parsed_image_B8A, parsed_image_B11)
+    return JsonResponse(map_data)
