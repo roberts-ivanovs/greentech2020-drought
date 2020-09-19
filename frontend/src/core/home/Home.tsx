ce8f7092df47515be5966e99e3fac97d8c5de795
Arina Solovjova <arinacka2@gmail.com>
2020-09-19 14:25:30 +0300

    Add latraps

--- frontend/src/core/home/Home.tsx
+++ frontend/src/core/home/Home.tsx
@@ -71,14 +71,14 @@ export function Home({ }: Props): ReactElement {
             <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
           </div>
           <div className="col-lg-4">
-            <img className="rounded mx-auto d-block" src="planet-earth.png" alt="Satalite" width="140" height="140" />
+            <img className="rounded mx-auto d-block" src="planet-earth.png" alt="Planet earth" width="140" height="140" />
             <h2 style={{ textAlign: "center", paddingTop: "5mm" }}>Worldwide</h2>
             <p>Data can be collected from all places on earth. By examining the data from previous years of set territory,
               we can give you our predictions about the lands quality.</p>
             <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
           </div>
           <div className="col-lg-4">
-            <img className="rounded mx-auto d-block" src="money-tree.png" alt="Satalite" width="140" height="140" />
+            <img className="rounded mx-auto d-block" src="money-tree.png" alt="Money tree icon" width="140" height="140" />
             <h2 style={{ textAlign: "center", paddingTop: "5mm" }}>Investment</h2>
             <p>By predicting land quality for the nearest future, you invest now and save money later by being prepared
                for natural circumstances.</p>
@@ -90,11 +90,11 @@ export function Home({ }: Props): ReactElement {
 
         <div className="row featurette">
           <div className="col-md-7">
-            <h2 className="featurette-heading">First featurette heading. <span className="text-muted">It’ll blow your mind.</span></h2>
+            <h2 className="featurette-heading">LPKS LATRAPS <span className="text-muted">It’ll blow your mind.</span></h2>
             <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
           </div>
           <div className="col-md-5">
-            <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee" /><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+            <img className="rounded mx-auto d-block" src="latraps.jpg" alt="Latraps logo" width="250" height="250" />
           </div>
         </div>
 
